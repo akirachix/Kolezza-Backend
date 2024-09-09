e9835a0bd511b57c8acd0ddd7f05c35f6a18ca3d
Mary-Njiru <marynwanjiruu@gmail.com>
2024-09-09 11:29:55 +0300

    removes comments

--- kolezzaSawaTok/api/serializers.py
+++ kolezzaSawaTok/api/serializers.py
@@ -1,16 +1,4 @@
-# from rest_framework import serializers
-# from users.models import User
 
-
-# class UserSerializer(serializers.ModelSerializer):
-#     password = serializers.CharField(write_only=True)  # Ensure password is write-only
-
-#     class Meta:
-#         model = User
-#         fields = ['id', 'username', 'first_name', 'last_name', 'password']  
-
-
-# api/serializers.py
 
 from rest_framework import serializers
 from users.models import User
